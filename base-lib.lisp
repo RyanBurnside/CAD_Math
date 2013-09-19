aad56683b9ccc4c3c847610eed4c7a37f36dd1bb
RyanBurnside <pixeloutlaw@gmail.com>
2013-09-18 23:09:42 -0600

    Added some functionality, planned some TODO goals

--- base-lib.lisp
+++ base-lib.lisp
@@ -1,9 +1,7 @@
 ;;;; Ryan Burnside 2013
 ;;;; Base Library for Common Lisp line and math functions
 
-
 ;;; Unit conversions
-
 (defun degrees-to-radians (degrees)
   (* degrees 0.017453))
 
@@ -11,30 +9,39 @@
   (* radians 57.2957795))
 
 ;;; Linear Operations
-
 (defun solve-x (y1 mx1 b1 y2 mx2 b2)
+  "Find the x value for two lines intersecting (sub function)"
   (/ (- (/ b2 y2) (/ b1 y1)) 
      (- (/ mx1 y1) (/ mx2 y2))))
 
 (defun solve-intersect (y1 mx1 b1 y2 mx2 b2)
+  "Find intersect using y = mx + b"
   (let ((x (solve-x y1 mx1 b1 y2 mx2 b2))) 
     (list x (/ (+ (* x mx1) b1) y1))))
 
-(defun distance (x y z x2 y2 z2)
+(defun distance (x y x2 y2)
   "Distance between point x y z and point x2 y2 z2 using sqrt(A^2 + B^2 + C^2)"
   (sqrt (+ (expt (- x2 x) 2)  ; A^2
-	   (expt (- y2 y) 2)  ; B^2
-	   (expt (- z2 z) 2)))) ; C^2
+	   (expt (- y2 y) 2))))  ; B^2
 
-(defun percent-between (x y z x2 y2 z2 percent)
+(defun percent-between (x y x2 y2 percent)
   "Returns a (x y x) list for the point at a fraction between two points"
   (list (+ x (* (- x2 x) percent))
-	(+ y (* (- y2 y) percent))
-	(+ z (* (- z2 z) percent))))
+	(+ y (* (- y2 y) percent))))
 
-(defun midpoint (x y z x2 y2 z2)
+(defun midpoint (x y x2 y2)
   "Returns a (x y x) list for the point at a 50 % between two points"
-  (percent-between x y z x2 y2 z2 .5))
+  (percent-between x y x2 y2 .5))
+
+;TODO look into meaning of atan in lisp as atan2 doesn't exist
+
+;(defun angle-radians (x y x2 y2)
+;  "Return the angle in degrees between points"
+;  (atan2 (- y2 y) (- x2 x)))
+
+;(defun angle-degrees (x y x2 y2)
+;  "Return the angle in degrees between points"
+;  (radians-to-degrees (angle-radians x y x2 y2)))
 
 (defun angular-offset (x y length direction)
   (list (+ x (* (cos direction) length))
@@ -43,19 +50,24 @@
 ;;; Circular / Arc operations
 ;;  Unless specified these opreations return a circle in the form 
 ;;  (center-x center-y radius)
-
 (defun 1-point-circle(x y radius)
   "Really dumb but here for consistancy"
   (list x y radius))
 
 (defun 2-point-circle(x y x2 y2)
   "Circle defined by a line and it's midpoint"
-  (let* ((m (midpoint x y 0 x2 y2 0))
+  (let* ((m (midpoint x y x2 y2))
 	 (center-x (car m))
 	 (center-y (cadr m)))
-    (list center-x center-y (abs (distance center-x center-y 0 x2 y2 0)))))
+    (list center-x center-y (abs (distance center-x center-y x2 y2)))))
+
+;(defun 3-point-circle(x y x2 y2 x3 y3)
+;  "Find a circle that goes through 3 points"
+;  (let ((delta-1 (
 
 ;;; Polyline/Ngon operations
+;;; Note that 'in' means within a border of a shape
+;;; Note that 'on' means the point may lie on the border or in
 
 ;;Heavily test function below!
 (defun point-in-triangle(px py p1x p1y p2x p2y p3x p3y)
@@ -70,13 +82,17 @@
 	 (alpha (/ (+ (* c a) (* d e)) (+ (* c b) (* d f))))
 	 (beta (/ (+ (* g a) (* b e)) (+ (* c b) (* d f))))
 	 (gamma (- (- 1.0 alpha) beta)))
-    (if (and (> alpha 0.0) 
-	     (> beta 0.0)
-	     (> gamma 0.0))
-	t
-	nil)))
+    (and (> alpha 0.0) (> beta 0.0) (> gamma 0.0))))
 
 (defun point-in-rectangle (px py x1 y1 x2 y2)
   "Use point px py and two verts upper left (x1 y1) and lower right (x2 y2)"
-  (if (or (<= px x1)(<= py y1)(>= px x2)(>= py y2)) nil t))
-      
+  (not (or (<= px x1)(<= py y1)(>= px x2)(>= py y2))))
+
+(defun point-in-circle (px py center-x center-y radius)
+  "Test to see if a point is within a circle"
+  (> radius (distance px py center-x center-y)))
+
+;TODO
+;defun point-in-thick-line (useful for picking lines)
+;defun point-in-polygon (useful for picking polygons)
+
